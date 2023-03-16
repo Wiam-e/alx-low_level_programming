50f8c03c7acffe81605994ddaf81f39eda7ba2f3
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-16 10:54:12 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+*Writing first C program
+*main program using puts function to print a line
+*return results: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
