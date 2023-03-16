334207a90577e7ca831b98c8a81e46fbc2711af7
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-16 11:22:15 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - a program that uses printf function to print a line
+*return: 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
