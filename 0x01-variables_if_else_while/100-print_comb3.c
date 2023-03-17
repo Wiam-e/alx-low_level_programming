af43994dbd0c5452a9ff0b740c39faeaa005a900
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-17 13:32:52 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - a program that prints all possible different combinations of two digits
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; i++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if(i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
