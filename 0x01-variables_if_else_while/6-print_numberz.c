e74cd374d266fa3ae157f8f2f500f8d45efc9a09
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-17 11:52:15 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - program that prints single digit numbs starting with 0
+*Retur: 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
