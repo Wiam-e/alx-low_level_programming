9e55f5f15e2de3da6e546fba1d7a9a67c2c4171c
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-17 14:17:41 -0700

    Combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - a program that prints all possible different combinations of two digits
+*main - a program that prints all possible diff combos of two digits
 *Return: 0 (Success)
 */
 int main(void)
