423a30545af066e71c68c77bd4a752c2fe6e021e
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-21 14:45:36 -0700

    Alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_alphabet - it prints all alphabets in lowercase
+*/
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+_putchar('\n');
+}
