36b0e4d6d62eda7eefd899d81972ab373d4b1277
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-22 13:16:41 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
-*main -  is a test for a postitive or negative function
-*Return: 0 (Success)
+*main - is a test for a postitive or negative function
+*Return: 0
 */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
