e9175edabd8479a12d7a99a1bdd70cc5fca37961
Wiam Errabih <wiam.errabih@gmail.com>
2023-03-23 09:35:03 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*  mul - a function that multiplies two integers
+* @a: first num input
+* @b: second num input
+* Return: result of the 2 inputs
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,12 @@
 int _putchar(char c);
 int _isupper(int i);
 int _isdigit(int i);
+int mul(int a, int b);
+void print_numbers(void);
+void print_numbers(void);
+void print_numbers(void);
+void print_line(int n);
+void print_line(int n);
+void print_square(int size);
+void print_triangle(int size);
 #endif
