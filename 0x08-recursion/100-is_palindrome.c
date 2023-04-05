a8c91cc1a67ee57dcdcf27802737c7347cea1e60
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-05 11:36:10 -0700

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,18 @@
 #include "main.h"
 /**
-* _strlen_recursion - function that returns the length of a string
+* is_palindrome - function that tests if a string is a palindrome
 * @s: string
+* Return: 1 if string is palindrome, else 0.
+*/
+int is_palindrome(char *s)
+{
+if (*s == 0)
+return (1);
+return (test_pal(s, 0, _strlen_recursion(s)));
+}
+/**
+* _strlen_recursion - returns the length of a string
+* @s: string to calculate the length 
 * Return: length of the string
 */
 int _strlen_recursion(char *s)
@@ -11,28 +22,17 @@ return (0);
 return (1 + _strlen_recursion(s + 1));
 }
 /**
-* test_pal - function to test the string recursively if palindrome
-* @s: string 
+* test_pal - function that tests string recursively if it is palindrome
+* @s: string tested
+* @i: iterator
 * @l: length of the string
-*@i: iterator
-*Return: 1 if palindrome, else 0.
+* Return: 1 if palindrome, otherwise 0
 */
-int test_pal(char *s, int l, int i)
+int test_pal(char *s, int i, int l)
 {
 if (*(s + i) != *(s + l - 1))
 return (0);
-else if (i >= l)
+if (i >= l)
 return (1);
 return (test_pal(s, i + 1, l - 1));
-}
-/**
-* is_palindrome - function that tests if string is palindrome
-* @s: string
-* Return: 1 if string is palindrome, else 0
-*/
-int is_palindrome(char *s)
-{
-if (*s == 0)
-return (1);
-return (test_pal(s, 0, _strlen_recursion(s)));
 }
