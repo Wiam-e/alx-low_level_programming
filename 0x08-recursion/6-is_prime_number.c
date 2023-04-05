cd663316a2ba33f1bb5b58387c771d7cc6b1b9e4
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-05 10:27:53 -0700

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,31 @@
 #include "main.h"
 /**
-* is_prime_number - determines if integer is a prime number
-* @n: number to test
-* Return: 1 if n is a prime number, else 0.
-*/
-int is_prime_number(int n)
-{
-if (n <= 1)
-return (0);
-else
-return (test_pnmbr(n, n - 1));
-}
-/**
-* test_pnmbr - recursive function tests if a number is prime 
+* test_pnmbr - recursive function tests if a number is primary
 * @n: number to test
 * @i: iterator
-* Return: 1 if n is prime, else 0.
+* Return: 1 if n is primary, else, 0.
 */
 int test_pnmbr(int n, int i)
 {
-if (n % i == 0 && i > 0)
+if (i > 1)
+{
+if (n % i == 0)
 return (0);
-else if (i == 1)
-return (1);
 else
 return (test_pnmbr(n, i - 1));
+}
+else
+return (1);
+}
+/**
+* is_prime_number - determines if integer is primary
+* @n: number to test
+* Return: 1 if n is a primary, else 0.
+*/
+int is_prime_number(int n)
+{
+if (n < 0 || n == 1)
+return (0);
+else
+return (test_pnmbr(n, n - 1));
 }
