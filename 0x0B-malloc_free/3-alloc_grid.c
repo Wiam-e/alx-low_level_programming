914af2c88c985cca045c9cab6408240e6555f0c5
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-11 13:31:07 -0700

    Malloc Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,35 +8,25 @@
 */
 int **alloc_grid(int width, int height)
 {
-int **g2;
-int i, j = 0;
+int **g;
+int i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
-g2 = malloc(height * sizeof(*g2));
-
-if (g2 == NULL)
+g = malloc(height * sizeof(*g));
+if (g == NULL)
 return (NULL);
-
 for (i = 0; i < height; i++)
-g2[i] = malloc(width * sizeof(**g2));
 {
-if (g2[i] == NULL)
+g[i] = malloc(width * sizeof(**g));
+if (g[i] == NULL)
 {
-{
-free(g2);
+for (; i >= 0; i--)
+free(g[i]);
+free(g);
 return (NULL);
 }
-for (j = 0; j <= i; j++)
-{
-free(g2[j]);
-return (NULL);
-}
-}
-
 for (j = 0; j < width; j++)
-{
-g2[i][j] = 0;
+g[i][j] = 0;
 }
-}
-return (g2);
+return (g);
 }
