9e78c0888c162274d2b2129cf374cb2fc3549dc8
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-11 11:35:12 -0700

    Malloc Free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 char *c;
 int i, j, k, l;
 if (s1 == NULL)
-s1 = "";
+s1 = "\0";
 if (s2 == NULL)
-s2 = "";
+s2 = "\0";
 for (i = 0; s1[i] != '\0'; i++)
 ;
 for (j = 0; s2[j] != '\0'; j++)
