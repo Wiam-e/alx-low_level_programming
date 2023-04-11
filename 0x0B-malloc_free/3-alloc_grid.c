fe001810a37fe35432e5ec0804ffdbb5e9c9e211
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-11 11:41:32 -0700

    Malloc Free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* alloc_grid - create a 2d integer grid using malloc.
+* @width: int size width
+* @height: int size height
+* Return: Pointer to new grid, otherwise Null if failed
+*/
+int **alloc_grid(int width, int height)
+{
+int **g;
+int i = 0, j = 0;
+if (width + height < 2 || width < 1 || height < 1)
+return (NULL);
+
+g = malloc(height * sizeof(*g));
+if (g == NULL)
+return (NULL);
+
+while (i < height)
+{
+g[i] = malloc(width * sizeof(**g));
+if (g[i] == NULL)
+{
+for (i--; i >= 0; i--)
+free(g[i]);
+free(g);
+return (NULL);
+}
+i++;
+
+while (j < width)
+g[i][j] = 0;
+j++;
+}
+return (g);
+}
