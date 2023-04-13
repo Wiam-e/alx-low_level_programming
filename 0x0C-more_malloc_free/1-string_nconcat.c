e12817d0293ffe61346449bf4593a1dbddf8ab0b
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-13 10:24:25 -0700

    More Malloc Free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,31 +10,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *c;
-unsigned int i, j, l1, l2;
-j = 0;
-while (s1 && s1[l1])
-{
+int i, j, l1 = 0, l2 = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+while (s1[l1] != '\0')
 l1++;
-}
-while (s2 && s2[l2])
-{
+while(s2[l2] != '\0')
 l2++;
-}
+
 if (n >= l2)
 {
 n = l2;
 c = malloc(sizeof(char) * (l1 + l2 + 1));
 }
 else
-c = malloc (sizeof(char) * (l1 + n + 1));
+c = malloc(sizeof(char) * (l1 + n + 1));
 if (c == NULL)
 return (NULL);
-
-for (i = 0; i < l1 ; i++)
+for (i = 0; i < l1; i++)
 {
 c[i] = s1[i];
 }
-while (j < n)
+for (j = 0; j < n; j++)
 {
 c[i++] = s2[j];
 }
