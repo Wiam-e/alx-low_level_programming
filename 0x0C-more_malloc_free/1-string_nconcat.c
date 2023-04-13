4d32db9538ca3aaa570c907d3538803cef561195
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-13 10:30:03 -0700

    More Malloc Free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,15 +10,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *c;
-int i, j, l1 = 0, l2 = 0;
+unsigned int i, j, l1 = 0, l2 = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-for (len1 = 0; s1[len1] != '\0'; len1++)
+for (l1 = 0; s1[l1] != '\0'; l1++)
 ;
-for (len2 = 0; s2[len2] != '\0'; len2++)
+for (l2 = 0; s2[l2] != '\0'; l2++)
 ;
 
 if (n >= l2)
