6af91d3ac9691c944c749dd92e831fdc04e67bb9
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-27 11:28:52 -0700

    Hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
 section .data
-msg db "Hello, Holberton", 0
-fmt db "%s", 10, 0
-
+	msg db "Hello, Holberton", 10
+	
 	section .text
 	extern printf
 	global main
 main:
-	mov rsi, msg
-	mov rdi, fmt
 	mov rax, 0
+	mov rdi, 0
+	mov rsi, msg
+	mov rdx, 13
 	call printf
 
 	mov rax, 0
