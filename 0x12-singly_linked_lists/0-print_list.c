1942af12ba2bb1ca5658673b54655575c3bec565
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-27 10:01:05 -0700

    Update Print List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
-	
-	while (h != NULL)
-	{
+size_t s = 0;
+
+while (h != NULL)
+{
 	if (h->str == NULL)
 	printf("[%d] %s\n", 0, "(nil)");
 	else
 	printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	s++;
-	}
+}
 	return (s);
 }
