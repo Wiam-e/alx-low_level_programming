e05fc8cc9ded9fe9583a10b4dde90ee0ce2df676
Wiam Errabih <wiam.errabih@gmail.com>
2023-04-27 10:24:23 -0700

    Update Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_list - function about freeing a linked list
+ * free_list - function that frees a linked list
  * @head: head of linked list
  */
 void free_list(list_t *head)
 {
-	list_t *h, *fr; /* fr to free next */
-	h = head;
+  list_t *h;
 
+	h = head;
 	while (h != NULL)
 	{
-		fr = h->next;
+		head = head->next;
 		free(h->str);
 		free(h);
-		h = fr;
 	}
 }
