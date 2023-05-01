4662279f5680c8fc0707e77c87150892af0098aa
Wiam Errabih <wiam.errabih@gmail.com>
2023-05-01 10:01:46 -0700

    Fix error: Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,11 +20,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-	i = 0;
-	while (i < idx - 1 && head != NULL)
+	while (i = 0; i < idx - 1 && head != NULL; i++)
 		{
 		h = h->next;
-		i++;
 		}
 	}
 
