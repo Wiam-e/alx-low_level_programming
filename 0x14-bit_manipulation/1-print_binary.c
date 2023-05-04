808aad5d7d7c4eecadcbf4eb02d54eaf293cbda7
Wiam Errabih <wiam.errabih@gmail.com>
2023-05-04 08:38:47 -0700

    Number in binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * print_binary - function that prints the binary representation of a number
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1, otherwise -1 (Error).
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_binary - function that prints a number in binary.
  * @n: decimal number.
  */
 void print_binary(unsigned long int n)
@@ -13,8 +25,8 @@ void print_binary(unsigned long int n)
 	}
 	else
 	{
-		if (n >> 1)
-		print_binary(n >> 1);
-		printf("%lu", n & 1);
+	if (n >> 1)
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 	}
 }
